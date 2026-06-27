59468763f362165927c4389ef19bf241365b5cb7
clone-maddy <mpm.agrawal.48@gmail.com>
2026-06-27 21:38:43 +0530

    feat: connect MongoDB to backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config();
 
@@ -9,6 +10,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected successfully!'))
+  .catch((err) => console.log('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('Trading App Backend is running!');
